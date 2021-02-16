d8cf195b76a809edb019d3a16cb432d8ec217b94
Vladislav Baranov <vladislavbaranov1996@gmail.com>
2021-02-16 22:11:21 +0300

    Fixed all "main"

--- main.py
+++ main.py
@@ -3,39 +3,25 @@ from activetime import ActiveTime
 from datetime import datetime
 import time
 
-current_window = ""
-all_active_windows = []
-spent_time = []
-start = datetime.now()
-
 
 def main():
-    global current_window, start
-    new_window = active_foreground()
-
-    if current_window != new_window:
-        stop = datetime.now()
-        activity = ActiveTime(start, stop)
-
-        if current_window not in all_active_windows:
-            all_active_windows.append(current_window)
-            spent_time.append(activity.get_diff_time())
-        else:
-            spent_time[all_active_windows.index(current_window)] += activity.get_diff_time()
-
-        index_current_windows = all_active_windows.index(current_window)
-        if activity.time_format(spent_time[index_current_windows]) != '0 seconds':
-            print(f'Previous window: {current_window} - \
-{activity.time_format(spent_time[index_current_windows])}.')
-
-        current_window = new_window
-        start = datetime.now()
-
-        print(f'Current window: {current_window}.')
-        print()
-
-
-if __name__ == "__main__":
+    active_window = ''
+    activities_cache = {}
+    start_time = datetime.now()
     while True:
-        main()
+        new_window = active_foreground()
+        if active_window != new_window:
+            stop_time = datetime.now()
+            activity = ActiveTime(start_time, stop_time)
+            if active_window not in activities_cache:
+                activities_cache[active_window] = activity.get_diff_time()
+            else:
+                activities_cache[active_window] += activity.get_diff_time()
+            if active_window:
+                print(f'{active_window} - {activities_cache[active_window]}')
+            active_window = new_window
+            start_time = datetime.now()
         time.sleep(1)
+
+
+main()
