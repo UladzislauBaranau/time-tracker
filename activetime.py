902cdc53252085e85476d717fca90497a881d0ac
Vladislav Baranov <vladislavbaranov1996@gmail.com>
2021-02-28 20:29:49 +0300

    Add class CorrectTimeFormat and changed class ActiveTime

--- activetime.py
+++ activetime.py
@@ -1,25 +1,36 @@
+class CorrectTimeFormat:
+
+    def __init__(self, time):
+        self.hours = time.seconds // 3600
+        self.minutes = (time.seconds - self.hours * 3600) // 60
+        self.seconds = time.seconds - self.hours * 3600 - self.minutes * 60
+
+    def serialize_total_time(self):
+        if self.hours == 0 and self.minutes == 0:
+            return f'{self.seconds} seconds'
+        elif self.hours == 0:
+            return f'{self.minutes} minutes, {self.seconds} seconds'
+        return f'{self.hours} hours, {self.minutes} minutes, {self.seconds} seconds'
+
+    def serialize_time_interval(self):
+        return f'{self.hours}h {self.minutes}min {self.seconds}sec'
+
+
 class ActiveTime:
 
-    def __init__(self, start_activity, stop_activity):
-        self._start = start_activity
-        self._stop = stop_activity
+    def __init__(self, start, stop):
+        self._start = start
+        self._stop = stop
 
     def get_diff_time(self):
         return self._stop - self._start
 
-    def time_format(self, active_time):
-        days = active_time.days
-        hours = active_time.seconds // 3600
-        minutes = (active_time.seconds - hours * 3600) // 60
-        seconds = active_time.seconds - hours * 3600 - minutes * 60
+    @staticmethod
+    def get_total_time(total_time):
+        return CorrectTimeFormat(total_time).serialize_total_time()
 
-        if days == 0 and hours == 0 and minutes == 0:
-            return f'{seconds} seconds'
-        elif days == 0 and hours == 0:
-            return f'{minutes} minutes, {seconds} seconds'
-        elif days == 0:
-            return f'{hours} hours, {minutes} minutes, {seconds} seconds'
-        else:
-            return f'{days} days, {hours} hours, {minutes} minutes, {seconds} seconds'
+    @staticmethod
+    def get_time_interval(time_interval):
+        return CorrectTimeFormat(time_interval).serialize_time_interval()
 
 
