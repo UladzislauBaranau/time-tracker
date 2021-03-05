91f43af5306bd67377638e5ebfb88d409ff9e8ac
Vladislav Baranov <vladislavbaranov1996@gmail.com>
2021-03-05 22:15:38 +0300

    Added method 'serialize_for_json' to class 'CorrectTimeFormat'

--- activetime.py
+++ activetime.py
@@ -5,15 +5,22 @@ class CorrectTimeFormat:
         self.minutes = (time.seconds - self.hours * 3600) // 60
         self.seconds = time.seconds - self.hours * 3600 - self.minutes * 60
 
-    def serialize_total_time(self):
+    def serialize_time_interval(self):
         if self.hours == 0 and self.minutes == 0:
             return f'{self.seconds} seconds'
         elif self.hours == 0:
-            return f'{self.minutes} minutes, {self.seconds} seconds'
-        return f'{self.hours} hours, {self.minutes} minutes, {self.seconds} seconds'
+            return f'{self.minutes} minutes {self.seconds} seconds'
+        return f'{self.hours} hours {self.minutes} minutes {self.seconds} seconds'
 
-    def serialize_time_interval(self):
+    def serialize_total_time(self):
         return f'{self.hours}h {self.minutes}min {self.seconds}sec'
+
+    def serialize_for_json(self):
+        return {
+            'hours': self.hours,
+            'minutes': self.minutes,
+            'seconds': self.seconds
+        }
 
 
 class ActiveTime:
@@ -26,11 +33,9 @@ class ActiveTime:
         return self._stop - self._start
 
     @staticmethod
-    def get_total_time(total_time):
-        return CorrectTimeFormat(total_time).serialize_total_time()
-
-    @staticmethod
     def get_time_interval(time_interval):
         return CorrectTimeFormat(time_interval).serialize_time_interval()
 
-
+    @staticmethod
+    def get_total_time(total_time):
+        return CorrectTimeFormat(total_time).serialize_total_time()
