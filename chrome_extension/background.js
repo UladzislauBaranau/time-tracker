93add314ad1596f61add22a46c19f4e323bf81b3
Vladislav Baranov <vladislavbaranov1996@gmail.com>
2021-03-25 15:15:13 +0300

    Initial commit

---
+++ chrome_extension/background.js
@@ -0,0 +1,29 @@
+function sendCurrentURL() {
+    chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
+        var tabURL = tabs[0].url;
+        console.log(tabURL);
+
+        var sendObject = {
+            'CurrentURL': tabURL
+        };
+        var json = JSON.stringify(sendObject);
+
+        var xhr = new XMLHttpRequest();
+
+        xhr.open('POST', 'http://127.0.0.1:5000/');
+        xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+        // occurs only when request cannot be executed
+        xhr.onerror = function() {
+            alert('Request Error');
+        };
+        xhr.send(json);
+    });
+}
+
+
+setInterval(sendCurrentURL, 1500)
+
+
+
+
