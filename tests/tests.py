23d403bc8436801b54e48adbce8cc28cf431ee04
Vladislav Baranov <vladislavbaranov1996@gmail.com>
2021-04-11 15:48:57 +0300

    Added class 'ActivityTests'

--- tests/tests.py
+++ tests/tests.py
@@ -1,9 +1,10 @@
-from unittest import TestCase
+import unittest
 from activetime import ActiveTime, TimeError
+from activity import Activity, ActivitiesStorage
 from datetime import datetime, timedelta
 
 
-class TimeTests(TestCase):
+class TimeTests(unittest.TestCase):
 
     def test_correct_active_time(self):
         start = datetime.now()
@@ -15,7 +16,6 @@ class TimeTests(TestCase):
         stop = start - timedelta(hours=2, minutes=45)
         with self.assertRaises(TimeError) as context:
             ActiveTime(start, stop)
-
         self.assertTrue('\nSomething is wrong with your time. Check the current time\n' in str(context.exception))
 
     def test_active_time_more_than_five_hours(self):
@@ -36,3 +36,37 @@ class TimeTests(TestCase):
         active_time = ActiveTime(start, stop)
         self.assertEqual(active_time.get_diff_time(), (stop - start))
 
+
+class ActivityTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.activities = ActivitiesStorage()
+        cls.a = Activity('title', datetime.now() - timedelta(hours=1, minutes=15), datetime.now())
+        cls.a2 = Activity('title2', datetime.now() - timedelta(minutes=4, seconds=23), datetime.now())
+
+        cls.activities2 = ActivitiesStorage()
+        cls.activities2.storage = [
+            {
+                'active_tab': 'title3',
+                'active_sessions': [{'hours': 2, 'minutes': 20, 'seconds': 0}],
+                'last_active_session': {'hours': 2, 'minutes': 20, 'seconds': 0},
+                'total_time': timedelta(seconds=8400, microseconds=11)
+            }
+        ]
+        cls.a3 = Activity('title3', datetime.now() - timedelta(minutes=12, seconds=45), datetime.now())
+
+    def test_add_activity(self):
+        self.activities.add_activity(self.a)
+        self.activities.add_activity(self.a2)
+        self.assertIn('title', self.activities.storage[0]['active_tab'])
+        self.assertIn('title2', self.activities.storage[1]['active_tab'])
+
+    def test_update_activity(self):
+        self.activities.update_activity(self.activities2.storage[0], self.a3)
+        self.assertEqual(len(self.activities2.storage[0]['active_sessions']), 2)
+        self.assertEqual(len(self.activities2.storage), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
